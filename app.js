f0b8b6660ec3fb482782a0c2a8ab0bc9ff560621
swarga500 <swarga500@gmail.com>
2021-09-02 21:47:42 +0600

    error handle

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 const booksDiv = document.getElementById('books-div');
 const inputId = document.getElementById('input-field');
 const countId = document.getElementById('search-count');
-const errorDiv = document.getElementById('error-div')
+const errorDiv = document.getElementById('error-div');
+
 
 // onclick event handler
 const loadBooks = () =>{
   const searchText = inputId.value;
+
   // clear previous data 
   inputId.value = '';
   booksDiv.textContent = '';
@@ -15,16 +17,17 @@ const loadBooks = () =>{
     
     // error handling
     if(searchText === ''){
-      errorDiv.innerText = 'search field is empty'
+      errorDiv.innerText = 'Empty search can not make result';
       return
     }
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`;
+    
+    const url = ` https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => {
       // error handling------------
       if(data.numFound === 0){
-        errorDiv.innerText = 'result not found'
+        errorDiv.innerHTML = `<p class="text-danger">result not found</p>`
         return
       }
       else{
@@ -56,13 +59,13 @@ const displayBook = books =>{
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img height="350px" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img height="300px" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Name: <p class="text-muted fw-bold">${book.title}</p></h5>
-              <h5>Author: <p class="text-muted">${book.author_name[0]}</p></h5>
-              <h5>Publisher: <p class="text-muted">${book.publisher[0]}</p></h5>
+              <h5>Author: <p class="text-muted">${book.author_name?.[0]}</p></h5>
+              <h5>Publisher: <p class="text-muted">${book.publisher?.[0]}</p></h5>
               <h5>First publish: <p class="text-muted">${book.first_publish_year}</p></h5>
-              <h5>Publish date: <p class="text-muted">${book.publish_date[0]}</p></h5>
+              <h5>Publish date: <p class="text-muted">${book.publish_date?.[0]}</p></h5>
             </div>
             </div>
           </div>
